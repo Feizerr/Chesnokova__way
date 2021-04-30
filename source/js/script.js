6ad88c9e18e3aa0a191c246d20688f6e7c705815
Feizerr <anastasiapred@gmail.com>
2021-04-30 17:14:24 +0300

    Добавляет js для меню

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,32 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+// var pageHeader = document.querySelector('.page-header');
+// var headerToggle = document.querySelector('.page-header__toggle');
+
+// pageHeader.classList.remove('page-header--nojs');
+
+// headerToggle.addEventListener('click', function () {
+//   if (pageHeader.classList.contains('page-header--closed')) {
+//     pageHeader.classList.remove('page-header--closed');
+//     pageHeader.classList.add('page-header--opened');
+//   } else {
+//     pageHeader.classList.add('page-header--closed');
+//     pageHeader.classList.remove('page-header--opened');
+//   }
+// });
+
+const headerToggle = document.querySelector('.header__toggle');
+const navigation = document.querySelector('.header__navigation');
+const headerLogo = document.querySelector('.header__logo');
+
+headerToggle.classList.remove('visually-hidden');
+navigation.classList.remove('navigation-nojs')
+
+
+headerToggle.addEventListener('click', function () {
+  headerToggle.classList.toggle('header__toggle-closed');
+  headerLogo.classList.toggle('visually-hidden');
+  navigation.classList.toggle('navigation__show');
+});
 
 pageHeader.classList.remove('page-header--nojs');
 
